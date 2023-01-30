ab0853e971e110d6c7e8b2b60ce5acd4d9cb6c1d
NVH06 <nvh.fiverr@gmail.com>
2023-01-30 17:00:15 +0100

    test

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-import pandas as pd
-
-data = [10, 20, 30, 40, 50, 60]
-
-# Create the pandas DataFrame with column name is provided explicitly
-df = pd.DataFrame(data, columns=['Numbers'])
-
-# print dataframe.
-print(df)
+print("hello")
